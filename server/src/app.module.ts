5c710ddb0759c3b8b1c03614ea619fb9e63eec2a
caophuoclong <caophuoclong.se@gmail.com>
2023-02-06 14:58:19 +0700

    update entity and dto

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,14 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import {  MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ConfigsModule } from './configs/configs.module';
 import { ProductsModule } from './products/products.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigService } from '@nestjs/config';
+import { CustomerModule } from './customer/customer.module';
+import { ShopModule } from './shop/shop.module';
 
 @Module({
-  imports: [DatabaseModule, ConfigsModule, ProductsModule, AuthModule],
+  imports: [DatabaseModule, ConfigsModule, ProductsModule, AuthModule, CustomerModule, ShopModule],
   controllers: [],
   providers: [],
 })
